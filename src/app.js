d89a041658c730305e674b03c2b4ba25140886db
sergio19006 <sergiodphn@gmail.com>
2019-07-16 17:02:10 +0100

    Primera version de las subida de fotos, se suben correctamente a /home/sergio, pero no se ingresa la información en la base de datos

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import fileUpload from 'express-fileupload';
 const app = express();
 import userRouter from './infraestructure/routes/userRouter';
 import bookRouter from './infraestructure/routes/bookRouter';
 
+
+app.use(fileUpload());
 app.use(express.json());
 app.use(cors());
-//app.use('/users',userRouter);
+app.use('/users',userRouter);
 app.use('/books',bookRouter);
 
 
--- src/infraestructure/routes/bookRouter.js
+++ src/infraestructure/routes/bookRouter.js
@@ -1,14 +1,31 @@
-import { Router } from 'express';
-import { BookController } from '../controllers/bookController';
+import { Router } from "express";
+import { BookController } from "../controllers/bookController";
 
 const bookController = new BookController();
 
 const router = Router();
 
 router.post("/signupBook", async (req, res) => {
- // const response = await bookController.signup(req.body);
+  // const response = await bookController.signup(req.body);
   res.status(200).json("heyman");
 });
 
+router.post("/upload", function(req, res) {
+  if (Object.keys(req.files).length == 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+  
+  let epubFile = req.files.epubFile;
+  let imageFile = req.files.imageFile;
+  epubFile.mv(`/home/sergio/${req.files.epubFile.name}`, function(err) {
+    if (err) return res.status(500).send(err);
+    res.send("File uploaded!");
+  });
+
+  imageFile.mv(`/home/sergio/${req.files.imageFile.name}`, function(err) {
+    if (err) return res.status(500).send(err);
+    res.send("File uploaded!");
+  });
+});
 
 export default router;
